96df31a6dabb45f8fbf01c207eeefa8135bf61dd
codeme86 <pqxr1621@leeds.ac.uk>
2025-10-07 13:17:30 +0000

    update

--- tasks/task4_5/OddSum.kt
+++ tasks/task4_5/OddSum.kt
@@ -1,1 +1,15 @@
-// Task 4.5: summing odd integers with a for loop
+// Task 4.5: summing odd integers with a for loop 
+fun main() {
+    print("Enter the upper limit: ")
+    val limit = readln().toInt()
+
+    var sum: Long = 0
+
+    for(i in 1..limit) {
+        if (i % 2 != 0) {
+            sum += i 
+        } 
+    } 
+    println("Sum off odd numbers from 1 to $limit is $sum")
+}
+
--- tasks/task5_1_1/Anagrams.kt
+++ tasks/task5_1_1/Anagrams.kt
@@ -1,1 +1,14 @@
 // Task 5.1.1: anagram checking using a function
+fun rollDie(sides: Int) {
+    if (sides in setOf(4, 6, 8, 10, 12, 20)) {
+        println("Rolling a d$sides...")
+        val result = Random.nextInt(1, sides + 1)
+        println("You rolled $result")
+    }
+    else {
+        println("Error: cannot have a $sides-sided die")
+    }
+}
+
+fun anagrams(frist: String, second: String): Boolean {
+    val sorted1
